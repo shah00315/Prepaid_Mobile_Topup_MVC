f22b27584154236bff887d4ed6db268451581743
Sumanpreet Singh <ssumanpreet370@gmail.com>
2020-08-26 21:53:54 +1200

    Add project files.

---
+++ Prepaid_Mobile_Topup_MVC/Data/Migrations/20200223023807_Mobile1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prepaid_Mobile_Topup_MVC.Data.Migrations
+{
+    public partial class Mobile1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
